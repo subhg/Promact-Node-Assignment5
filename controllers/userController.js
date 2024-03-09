6f4fd94bb5dce79199fa96a69b2e41692bba5937
subhg <113555022+subhg@users.noreply.github.com>
2024-03-09 22:31:12 +0530

    added comments

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,38 +1,55 @@
-const mysql=require('mysql');
+// Import the MySQL module
+const mysql = require('mysql');
 
+// Create a MySQL connection
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Subham@02',
-    database: 'nodeassignment',
-  });
+    host: 'localhost',        
+    user: 'root',             
+    password: 'Subham@02',    
+    database: 'nodeassignment' 
+});
 
-  connection.connect();
+// Connect to the MySQL server
+connection.connect();
 
-  exports.listUsers=(req,res)=>{
-    connection.query('SELECT * FROM users',(err,results)=>{
+// Controller function to list users
+exports.listUsers = (req, res) => {
+    // Execute a SELECT query to retrieve all users from the 'users' table
+    connection.query('SELECT * FROM users', (err, results) => {
         if (err) throw err;
 
-        if(results.length>0){
-            res.render('users',{
-                title:'Users', users: results
+        
+        if (results.length > 0) {
+            res.render('users', {
+                title: 'Users',
+                users: results
             });
-        } else{
-            res.render('users',{title: 'Users', message: 'No users found'})
+        } else {
+            res.render('users', {
+                title: 'Users',
+                message: 'No users found'
+            });
         }
-    })
-  }
+    });
+};
 
-  exports.createUserForm=(req,res)=>{
-    res.render('create',{title:'Create User'});
-  };
+// Controller function to render the user creation form
+exports.createUserForm = (req, res) => {
+    res.render('create', {
+        title: 'Create User'
+    });
+};
 
-  exports.addUser =(req,res)=>{
-    const {userName}= req.body;
-    const user={ username: userName};
+// Controller function to add a new user to the 'users' table
+exports.addUser = (req, res) => {
+    const { userName } = req.body;
+    const user = {
+        username: userName
+    };
 
-    connection.query('INSERT INTO users Set?',user,(err)=>{
-        if(err) throw err;
+    // Execute an INSERT query to add the new user to the 'users' table
+    connection.query('INSERT INTO users SET ?', user, (err) => {
+        if (err) throw err;
         res.redirect('/users');
-    })
-  }
+    });
+};
