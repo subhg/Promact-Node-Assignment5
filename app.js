f1224cbf2dcac973edcac3a9cc3ac9fa90fdfde8
subhg <113555022+subhg@users.noreply.github.com>
2024-03-09 22:40:21 +0530

    added comments

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
-const express= require('express');
-const bodyParser= require ('body-parser');
-const app = express();
-const port=3000;
+// Import necessary modules
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: true}));
+// Create an Express application
+const app = express();
+
+// Set up middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// Set view engine and views directory
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-const mainController= require('./controllers/mainController.js');
-const userController= require('./controllers/userController');
+// Import controllers
+const mainController = require('./controllers/mainController.js');
+const userController = require('./controllers/userController');
 
-app.get('/',mainController.home);
-app.get ('/users',userController.listUsers);
+// Define routes
+app.get('/', mainController.home);
+app.get('/users', userController.listUsers);
 app.get('/create', userController.createUserForm);
-app.post('/add',userController.addUser);
+app.post('/add', userController.addUser);
 
+// 404 Page
 app.use((req, res) => {
     res.status(404).render('404', { title: '404 Page Not Found' });
-  });
+});
 
+// Start the server on specified port
+const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-  });
+});
